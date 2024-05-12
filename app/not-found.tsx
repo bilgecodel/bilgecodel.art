ee584c04de7c6fbcecfa87327af3f1efc7cf8fb6
Yılmaz Ev <yilmaz.ev@outlook.com>
2024-05-12 23:07:02 +0300

    conf: version upgraded, 404 page texts changed

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,10 +5,10 @@ export default function NotFound() {
         <div className="flex h-screen items-center justify-center">
             <div className="flex w-full max-w-xl flex-col items-center px-3">
                 <div className="mb-5 text-6xl font-semibold text-stone-700/25">404</div>
-                <h1 className="mb-3 text-center text-2xl font-semibold leading-[2.3rem] text-stone-700 sm:leading-[2.6rem] md:text-3xl md:leading-[3rem] lg:text-4xl lg:leading-[3.5rem]">Bu sayfaya ulaşılamıyor</h1>
-                <p className="text-center leading-[1.6rem] text-stone-500">Bağlantı bozuk olabilir veya sayfa kaldırılmış olabilir. Açmaya çalıştığın bağlantının doğru olup olmadığını kontrol et.</p>
+                <h1 className="mb-3 text-center text-2xl font-semibold leading-[2.3rem] text-stone-700 sm:leading-[2.6rem] md:text-3xl md:leading-[3rem] lg:text-4xl lg:leading-[3.5rem]">Something went wrong</h1>
+                <p className="text-center leading-[1.6rem] text-stone-500">The link may be broken or the page may have been removed. Check that the link you are trying to open is correct.</p>
                 <Link href="/" className="mt-5 flex cursor-pointer items-center gap-1.5 rounded-full bg-stone-800 px-5 py-3.5 transition duration-200 hover:bg-stone-900" aria-label="Ana Sayfaya Git">
-                    <p className="text-white">Ana Sayfaya Git</p>
+                    <p className="text-white">Go Home</p>
                 </Link>
             </div>
         </div>
